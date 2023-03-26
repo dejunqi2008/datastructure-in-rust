ae5e80ab7f9fd69640c1619fcb5f30b3fb59b7ca
dejunqi <dejunqi2008@gmail.com>
2023-03-25 22:20:28 -0700

    lc 019

--- src/leetcode/lc00019/solution.rs
+++ src/leetcode/lc00019/solution.rs
@@ -1,52 +1,79 @@
-use crate::leetcode::{utils::list_node::ListNode, lc00001::solution};
+use crate::leetcode::utils::list_node::{
+	ListNode,
+	print_list_helper,
+	create_linkedlist
+};
 
-
-pub struct Solution {
-	remove_nth_from_end: fn(Option<Box<ListNode>>) -> Option<Box<ListNode>>
-}
+pub struct Solution;
 
 impl Solution {
 
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-
-		let mut dummy: ListNode = ListNode::new(0);
+		if head.is_none() {
+			return head;
+		}
+		
+		let mut dummy = Box::new(ListNode::new(0));
 		dummy.next = head;
 
-		let mut dummy: Box<ListNode> = Box::new(dummy);
-		let mut  cur: Box<ListNode> = dummy.clone();
-		let mut prev: &mut ListNode = dummy.as_mut();
-
+		let mut cur = dummy.clone();
+		let mut prev = dummy.as_mut();
+		
 		for _ in 0..n {
 			cur = cur.next.unwrap();
 		}
-
-		while cur.next.is_some() {
-			cur = cur.next.unwrap();
-			prev = prev.next.as_mut().unwrap();
+		
+		loop {
+			match &cur.next {
+				None => break,
+				Some(_) => {
+					cur = cur.next.unwrap();
+					prev = prev
+							.next
+							.as_mut()
+							.unwrap();
+				}
+			}
 		}
 
-		let post = prev
-										.next
-										.as_mut()
-										.unwrap();
-		prev.next = post.next.clone();
+		prev.next = prev
+						.next
+						.as_mut()
+						.unwrap()
+						.next
+						.take();
 
 		return dummy.next;
 
     }
-	pub fn test() {
-		let arr = [5, 4, 3, 2, 1];
-		let mut head: Option<Box<ListNode>> = None;
-	
-		for i in 0..arr.len() {
-			let node: Box<ListNode> = Box::new(ListNode {
-				val: arr[i],
-				next: head.take()
-			});
-			head = Some(node);
-		}
 
-		// println!("{}", head.as_ref().unwrap().val);
-		Self::remove_nth_from_end(head, 2);
+
+	pub fn test() {
+		Self::case1();
+		Self::case2();
+		Self::case3();
+	}
+
+	fn case1() {
+		let arr = [5, 4, 3, 2, 1];
+		Self::share(&arr, 2);
+	}
+
+	fn case2() {
+		let arr = [1];
+		Self::share(&arr, 1)
+	}
+
+	fn case3() {
+		let arr = [2, 1];
+		Self::share(&arr, 1);
+	}
+
+	fn share(arr: &[i32], nth: i32) {
+		let head: Option<Box<ListNode>> = create_linkedlist(&arr);
+		print_list_helper(&head);
+		let newhead = Self::remove_nth_from_end(head, nth);
+		print_list_helper(&newhead);
+		println!("\n-------")
 	}
 }
--- src/leetcode/utils/list_node.rs
+++ src/leetcode/utils/list_node.rs
@@ -13,3 +13,31 @@ impl ListNode {
         };
     }
 }
+
+pub fn print_list_helper(head: &Option<Box<ListNode>>) {
+    let mut cur = head;
+    loop {
+        match cur {
+            None => break,
+            Some(node) => {
+                print!("{} -> ", node.val);
+                cur = &node.next
+            }
+        }
+    }
+    println!("None")
+}
+
+pub fn create_linkedlist(arr: &[i32]) -> Option<Box<ListNode>> {
+    // let arr = [5, 4, 3, 2, 1];
+    let mut head: Option<Box<ListNode>> = None;
+
+    for i in 0..arr.len() {
+        let node: Box<ListNode> = Box::new(ListNode {
+            val: arr[i],
+            next: head.take()
+        });
+        head = Some(node);
+    }
+    return head;
+}
