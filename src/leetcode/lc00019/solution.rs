c5fd599e2cd20eb494b5522654e2c57dc3f6771a
Dejun Qi <dqi@salesforce.com>
2023-04-27 07:10:59 -0700

    reforamat code

--- src/leetcode/lc00019/solution.rs
+++ src/leetcode/lc00019/solution.rs
@@ -9,41 +9,33 @@ pub struct Solution;
 impl Solution {
 
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-		if head.is_none() {
-			return head;
-		}
-		
-		let mut dummy = Box::new(ListNode::new(0));
-		dummy.next = head;
+        if head.is_none() {
+            return head;
+        }
 
-		let mut cur = dummy.clone();
-		let mut prev = dummy.as_mut();
-		
-		for _ in 0..n {
-			cur = cur.next.unwrap();
-		}
-		
-		loop {
-			match &cur.next {
-				None => break,
-				Some(_) => {
-					cur = cur.next.unwrap();
-					prev = prev
-							.next
-							.as_mut()
-							.unwrap();
-				}
-			}
-		}
+        let mut dummy = Box::new(ListNode::new(0));
+        dummy.next = head;
 
-		prev.next = prev
-						.next
-						.as_mut()
-						.unwrap()
-						.next
-						.take();
+        let mut cur = dummy.clone();
+        let mut prev = dummy.as_mut();
 
-		return dummy.next;
+        for _ in 0..n {
+            cur = cur.next.unwrap();
+        }
+
+        loop {
+            match &cur.next {
+                None => break,
+                Some(_) => {
+                    cur = cur.next.unwrap();
+                    prev = prev.next.as_mut().unwrap();
+                }
+            }
+        }
+
+        prev.next = prev.next.as_mut().unwrap().next.take();
+
+        return dummy.next;
 
     }
 
@@ -54,26 +46,26 @@ impl Solution {
 		Self::case3();
 	}
 
-	fn case1() {
-		let arr = [5, 4, 3, 2, 1];
-		Self::share(&arr, 2);
-	}
+    fn case1() {
+        let arr = [5, 4, 3, 2, 1];
+        Self::share(&arr, 2);
+    }
 
-	fn case2() {
-		let arr = [1];
-		Self::share(&arr, 1)
-	}
+    fn case2() {
+        let arr = [1];
+        Self::share(&arr, 1)
+    }
 
-	fn case3() {
-		let arr = [2, 1];
-		Self::share(&arr, 1);
-	}
+    fn case3() {
+        let arr = [2, 1];
+        Self::share(&arr, 1);
+    }
 
-	fn share(arr: &[i32], nth: i32) {
-		let head: Option<Box<ListNode>> = create_linkedlist(&arr);
-		print_list_helper(&head);
-		let newhead = Self::remove_nth_from_end(head, nth);
-		print_list_helper(&newhead);
-		println!("\n-------")
-	}
+    fn share(arr: &[i32], nth: i32) {
+        let head: Option<Box<ListNode>> = create_linkedlist(&arr);
+        print_list_helper(&head);
+        let newhead = Self::remove_nth_from_end(head, nth);
+        print_list_helper(&newhead);
+        println!("\n-------")
+    }
 }
